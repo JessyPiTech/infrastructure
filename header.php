87cfbc62988a68687512c40d477edf9614e61607
Casi <125995928+JessyPiTech@users.noreply.github.com>
2024-05-07 12:13:40 +0200

    supp de admin

--- header.php
+++ header.php
@@ -1,11 +1,4 @@
-<?php
-// Start or resume the session
-session_start();
-if (!isset($_SESSION['connect']) || !$_SESSION['connect']) {
-    header("Location: connecte.php"); 
-    exit(); 
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
